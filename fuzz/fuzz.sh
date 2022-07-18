cbfd61a178d5030e66a49dd081db3a408dbeb569
z2_ <88509734+z2-2z@users.noreply.github.com>
2022-07-18 15:21:22 +0200

    Lexer fuzzing results look good enough

--- fuzz/fuzz.sh
+++ fuzz/fuzz.sh
@@ -20,3 +20,7 @@ then
 fi
 
 cargo fuzz run "$target" -- -only_ascii=1 -max_len=128 -timeout=10 "$dictflag";
+
+# coverage report:
+# cargo fuzz coverage <target>
+# llvm-cov show -instr-profile=coverage/<target>/coverage.profdata ./target/x86_64-unknown-linux-gnu/release/<target> -show-line-counts-or-regions -show-instantiations -format=html -output-dir=coverage/<target>
